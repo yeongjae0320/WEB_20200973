cdf34e93ffd55432b7d81a1f8d11c9367ea1d1c0
김영재 <poweryjkim@naver.com>
2023-04-05 05:30:20 +0000

    웹프로그래밍

---
+++ js/basic_test.js
@@ -0,0 +1,27 @@
+// 한줄 주석 : 임시 자바스크립트 파일을 생성했습니다.
+var jb = 'hi';	// 변수 선언 뒤에 주석처리 가능
+/*
+여러 줄 주석: 여러 줄에 걸쳐 주석을 처리합니다.
+*/
+var a = 1;
+var b;
+b = 5;
+
+if (true) {
+	let c = 'let 접근';
+	var c_1 = 'var 접근';
+}
+
+//console.log(c) // Error?
+console.log(c_1);
+
+let d = 5;
+// let d = '값 재할당';	// Error?
+console.log(d);
+
+const e = '상수 1 접근';
+// e = 5;
+// const f // Error?
+console.log(e);
+
+}
--- js/search.js
+++ js/search.js
@@ -2,4 +2,13 @@ document.getElementById("search_btn").addEventListener('click', search_message);
 
 function search_message(){
    alert("검색을 수행합니다!");
+	
+	//변수에 저장
+	let search_str = document.querySelector("#search_txt");
+	
+	//태그에 값 추가
+	document.getElementById("search_message").innerHTML = search_str.value;
+	
+	//콘솔에 출력
+	console.log(search_str.value);
 }
