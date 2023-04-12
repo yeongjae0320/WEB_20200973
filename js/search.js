bfc867916829c2fd59acf69c2aa8128f1e0f8de0
김영재 <poweryjkim@naver.com>
2023-04-12 12:34:12 +0000

    웹프로그래밍

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-document.getElementById("search_btn").addEventListener('click', search_message);
+/*document.getElementById("search_btn").addEventListener('click', search_message);
 
 //변수에 저장
 let search_str = document.querySelector("#search_txt");
@@ -12,6 +12,33 @@ function search_message(){
 		alert("검색을 수행합니다!");
 		//태그에 값 추가
 		let text = document.getElementById("search_message").innerHTML = search_str.value;
+		document.querySelector("#form_main").submit();
+	}
+	
+	
+	//콘솔에 출력
+	//console.log(search_str.value);
+}*/
+
+// 검색 제한 단어
+const forbiddenWords = ["금지어1", "금지어2", "금지어3"];
+
+document.getElementById("search_btn").addEventListener('click', search_message);
+
+//변수에 저장
+let search_str = document.querySelector("#search_txt");
+
+function search_message(){
+	
+	if (search_str.value.length === 0) {
+		alert("검색어가 비었습니다. 입력해주세요");
+	} else if (forbiddenWords.includes(search_str.value)) {
+		alert("검색어가 제한 단어에 해당됩니다. 다른 검색어를 입력해주세요.");
+	} else {
+		alert("검색을 수행합니다!");
+		//태그에 값 추가
+		let text = document.getElementById("search_message").innerHTML = search_str.value;
+		// 검색어가 제한 단어가 아니면 폼 제출
 		document.querySelector("#form_main").submit();
 	}
 	
