29ca2b80233097ce54e63f81f2329ed0096f8bf7
김영재 <poweryjkim@naver.com>
2023-04-12 05:09:29 +0000

    웹프로그래밍

--- js/search.js
+++ js/search.js
@@ -1,6 +1,10 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
+//변수에 저장
+let search_str = document.querySelector("#search_txt");
+
 function search_message(){
+	
 	if (search_str.value.length === 0) {
 		alert("검색어가 비었습니다. 입력해주세요");
 	}
@@ -10,9 +14,6 @@ function search_message(){
 		let text = document.getElementById("search_message").innerHTML = search_str.value;
 		document.querySelector("#form_main").submit();
 	}
-	
-	//변수에 저장
-	//let search_str = document.querySelector("#search_txt");
 	
 	//콘솔에 출력
 	//console.log(search_str.value);
