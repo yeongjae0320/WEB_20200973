0566583a2e7b564e571f5dc3b7c44de9b7d5133f
김영재 <poweryjkim@naver.com>
2023-05-31 04:13:21 +0000

    웹프로그래밍(1)

--- js/session.js
+++ js/session.js
@@ -84,6 +84,7 @@ function session_join_set() { //세션 저장(객체)
 }
 
 //12주차 도전과제
+// !!!!! 복호화 구현하기 !!!!! security 가져와서, encryted
 function session_join_get() {
 	let storedValue = sessionStorage.getItem("Session_Storage_object");
 	console.log(storedValue);	//대박 ㅠㅠ난 천재다
