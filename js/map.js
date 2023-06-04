caa7066087e66915aedaacf62caa7e64943a42a9
김영재 <poweryjkim@naver.com>
2023-06-04 11:28:59 +0000

    웹프로그래밍(1)

--- js/map.js
+++ js/map.js
@@ -17,3 +17,35 @@ map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// 지도를 클릭한 위치에 표시할 마커와 인포윈도우를 생성합니다
+var marker = new kakao.maps.Marker(),
+    infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+// 추가구현
+// 지도를 클릭한 위치에 표시할 마커와 인포윈도우를 생성합니다
+var marker = new kakao.maps.Marker(),
+    infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+// 지도에 클릭 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+
+    // 클릭한 위도, 경도 정보를 가져옵니다
+    var latlng = mouseEvent.latLng;
+
+    // 마커 위치를 클릭한 위치로 옮깁니다
+    marker.setPosition(latlng);
+    marker.setMap(map);
+
+    // 인포윈도우에 표시될 HTML 내용을 생성합니다
+    var content =
+        '<div style="padding:10px; font-size:6px;">' +
+        '위도: ' + latlng.getLat() +
+        '<br>' +
+        '경도: ' + latlng.getLng() +
+        '</div>';
+
+    // 인포윈도우에 내용을 설정합니다
+    infowindow.setContent(content);
+    infowindow.open(map, marker);
+});
