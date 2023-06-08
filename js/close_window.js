ccd6ee5ad661b4ae51f3edea027613fd7ca7fccc
김영재 <poweryjkim@naver.com>
2023-06-08 08:54:02 +0000

    웹프로그래밍(1)

--- js/close_window.js
+++ js/close_window.js
@@ -9,6 +9,13 @@ function show_time() {
 	let divClock = document.getElementById("Time");
 	divClock.innerText = "남은 시간은 " + close_time2 + "초 입니다."; // 수정된 코드, 10초 삽입 시작
 	close_time2--;	//1초씩 감소
+	if (close_time2 == 5 - 1) {
+		//사실상 html에서는 10초 부터 카운트다운이 시작되지만,
+		//켜지고 1초 후 10초 카운트가 진행되는듯하다.
+		//그래서 5초일때 글씨를 빨갛게 바꾸기 위해 5 - 1을 해주었다.
+		divClock.style.color = "red";
+		divClock.style.fontSize = "50px"; // 폰트 크기 조절
+	}
 	setTimeout(show_time, 1000);	//1초마다 갱신
 }
 
--- js/pop_up.js
+++ js/pop_up.js
@@ -1,7 +1,7 @@
 function pop_up() {
 		var cookieCheck = getCookie("popupYN");
         if (cookieCheck != "N"){
-        	window.open("pop_up.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        	window.open("pop_up.html", "팝업테스트", "width=600, height=300, top=200, left=300");
         }
 }
 
