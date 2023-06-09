2b7fa1fe83e13a525c301b628a1e4e4fd4cfb257
김영재 <poweryjkim@naver.com>
2023-06-09 16:54:15 +0000

    웹프로그래밍(1)

--- js/session.js
+++ js/session.js
@@ -41,7 +41,7 @@ function session_get() {	//세션 읽기
 }
 
 function session_check() {	//세션 검사
-	if (sessionStorage.getItem("Session_Storage_test")) {
+	if (sessionStorage.getItem("Session_Storage_encrypted")) {
 		alert("이미 로그인 되었습니다.");
 		location.href = 'index_login.html';	//로그인 페이지로 이동
 	}
@@ -51,7 +51,7 @@ function session_del() {	//세션 삭제
 	//Check if the sessionStorage object exists
 	if (sessionStorage) {
 		//Retrieve data
-		sessionStorage.removeItem("Session_Storage_test");
+		sessionStorage.removeItem("Session_Storage_encrypted");
 		//alert("로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.");
 	} else {
 		alert("세션 스토리지 지원X");
